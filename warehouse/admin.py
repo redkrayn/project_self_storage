7f1cbac18a3637e088f2eaf16453a91139c9572a
fergoth <fergothew@gmail.com>
2025-07-29 14:54:56 +0300

    disable creating order via admin with active cells

--- warehouse/admin.py
+++ warehouse/admin.py
@@ -2,13 +2,25 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Order, Cell, Warehouse, Request
-
+from django.db.models import Exists, OuterRef
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ("cell", "start_date", "end_date", "adv_id")
     list_filter = ("adv_id",)
     search_fields = ("adv_id",)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "cell":
+            kwargs["queryset"] = Cell.objects.annotate(
+                has_order=Exists(
+                    Order.objects.filter(
+                        cell=OuterRef("pk"), is_active=True
+                    )
+                )
+            ).filter(has_order=False)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
 
 @admin.register(Cell)
 class CellAdmin(admin.ModelAdmin):
